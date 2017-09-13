cf16b0af1f5251c5ae5822a56c82112674bf5de9
Brittany Hall <30522928+brittanh@users.noreply.github.com>
2017-09-13 19:14:49 +0200

    Code Updates
    Update to code

--- Code/SimpleExample/objfun.py
+++ Code/SimpleExample/objfun.py
@@ -1,9 +1,7 @@
 #!/opt/local/bin/python
 # -*- encoding: ascii -*-
 """
-@purpose: Simple problem to test path-following algorithm
-          Taken from Kungurtsev et al 
-          ("SQP methods for parametric nonlinear optimization")
+@purpose: Simple problem to test optimization using path-following algorithm
 @author: Brittany Hall
 @date: 12.09.2017
 @version: 1
@@ -11,42 +9,49 @@
 """
 
 from casadi import *
-
+from numpy import array, empty
 #problem to solve
 # min p1x1^3+x2^2
 # st x2-exp(-x1)=>0, x1=> p2
 #want to find approximate solution at p=(8,1)
 
-#Approximate solution
-x0 = SX([0.5,0.6]) #numerical matrix
-y0 = SX([1.2])
-
-p = SX([1,-4])
-
-#Defining Constants and Variables
+#Defining Constants
 t = 0
-x = MX.sym('x',2,1)
-y = MX.sym('y',1)
-p = MX.sym('p',2,1)
+delta_t = 1e-4
+N = 1/delta_t
+alpha1 = 0.66 #0<alpha1<1
 
-#Defining objective function
-J = objfun('J',[x,p], [mtimes(p[0],x[0]**3)+x[1]**2])
+#Approximate solution
+x0 = array([0.5,0.6]) #numerical matrix
+p = empty([1, N])
+p[0] = array([-1, 4]) #initial parameter value
+p[N] = array([8,1]) #final parameter value
 
-#Defining constraints
-cont = constraint('cont',[x,p],[x[1]-exp(-x[0]),x[0]-p[1]])
+#initial variables from NLP
+chi = MX.sym('X',2,1) #primal variable
+lamb = MX.sym('lam',1) #dual variable
+mu = MX.sym(',mu',2,1) #dual variable
 
-#Calculating Derivative/gradient
-def df(x,y,p):
-    return gradient(objfun(x,y,p))
+#Calculate Lagrangian
 
-#Calculating Jacobian
-def Jacobian(x,y,p):
-    return jacobian()
-#Calculating optimality residual calculation
+#Calculate Jacobian of Lagrangian
 
+#Calculate gradient of F
 
-#Parametric SQP Method (path-following)
-def algo_pathfollowing:
-    while t<1:
-    #Calculate Active set
+#Path Following Algorithm
+##Algorithm 2 from 2016_Suwartadi_etal
 
+for k in range(1,N):
+    delta_p = p[k]-p[k-1] #calculating change in parameter p
+    #solve QP
+
+    #if QP is feasible:
+    if :
+        chi = chi + delta_chi
+        lamb = delta_lamb
+        mu = delta_mu
+        t = t + delta_t
+        k = k +1
+    else:
+        delta_t = alpha1*delta_t
+        t = t- delta_t
